06d9523b16f940dd060d9ad37e7cf2ff1f34521a
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-07-03 11:34:06 +0100

    Add some extra notes.

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,8 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     }
 }
 
+// This part fails to compile in `--release` (or with `-Copt-level=1`), unless you
+// also set `-Ccodegen-units=1`.
 core::arch::global_asm!(
     r#"
 
@@ -59,5 +61,31 @@ _start:
 
 "#
 );
+
+// This compiles OK, but the function isn't naked so it's technically incorrect
+// because the compiler could add a prologue that doesn't run because the stack
+// isn't setup yet. Also I think it's technically UB to run Rust code in a state
+// where the stack pointer is wrong.
+
+// #[no_mangle]
+// unsafe extern "C" fn _start() {
+//     core::arch::asm!("
+//     // Set stack pointer
+//     ldr r3, =stack_top
+//     mov sp, r3
+//     // Allow VFP coprocessor access
+//     mrc p15, 0, r0, c1, c0, 2
+//     orr r0, r0, #0xF00000
+//     mcr p15, 0, r0, c1, c0, 2
+//     // Enable VFP
+//     mov r0, #0x40000000
+//     vmsr fpexc, r0
+//     // Jump to application
+//     bl kmain
+//     // In case the application returns, loop forever
+//     b ."
+//     )
+// }
+
 
 // End of file
